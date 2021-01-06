2636ebcb418c568faa8150c2daf7225070bf5974
DosImpact <ypd03008@gmail.com>
2021-01-06 21:51:46 +0900

    cleanup

--- src/routes/Auth/section/Login.js
+++ src/routes/Auth/section/Login.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-import { Button, TextInput } from "components/common/Inputs/index";
-
 const Login = () => {
   return (
     <Container>
